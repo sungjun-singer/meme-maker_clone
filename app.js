111858b7d65784c2c8f9cb3cbf342e8418bbec0f
kimsungjun <kis07150@gmail.com>
2023-02-18 18:02:00 +0900

    마우스 눌렀을때 그리고 뗐을때 안그려지는 그림판 생성

--- app.js
+++ app.js
@@ -2,34 +2,26 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d")
 canvas.width = 800;
 canvas.height = 800;
+ctx.lineWidth = 2;
+let isPainting = false;
+function onMove(event){
+    if(isPainting){
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.fill();
+        return;
+    }
 
-const RAND_NUM = Math.floor(Math.random() * 800);
-let coord_x = 0;
-let coord_y = 0;
-
-const colors = [
-    "#ff3838",
-    "#ffb8b8",
-    "#c56cf0",
-    "#ff9f1a",
-    "#fff200",
-    "#32ff7e",
-    "#7efff5",
-]
-
-function onMouse(event){ // 마우스 이동했을때 동작하는 함수
-    ctx.beginPath(); // 경로시작.
-    ctx.moveTo(coord_x, coord_y); // 전역변수 coord_x, coord_y에서 시작.
-    const color = colors[Math.floor(Math.random() * colors.length)]; // 칼라는 칼라배열에서 하나 가져오고.
-    ctx.strokeStyle = color; // 선 색깔 가져온 칼라로 지정.
-    ctx.lineTo(event.offsetX, event.offsetY); // 현재x좌표,y좌표로 선 이동.
-    ctx.stroke(); // 선 긋기.
+    ctx.moveTo(event.offsetX, event.offsetY);
 }
 
-function onClick(event){
-    coord_x = event.offsetX;
-    coord_y = event.offsetY;
+function startPainting(){
+    isPainting = true;
 }
 
-canvas.addEventListener("click", onClick);
-canvas.addEventListener("mousemove", onMouse);
+function cancelPainting(){
+    isPainting = false;
+}
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting)
