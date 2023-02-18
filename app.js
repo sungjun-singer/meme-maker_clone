f1102dc63c8e57bfaeb64fac3c7c2d417742425b
kimsungjun <kis07150@gmail.com>
2023-02-18 17:33:24 +0900

    마우스 클릭시 시작점 바뀌는 코드 구현

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ const ctx = canvas.getContext("2d")
 canvas.width = 800;
 canvas.height = 800;
 
-let RAND_NUM = 0;
-
-setInterval(() => {
-    RAND_NUM = Math.floor(Math.random() * 800);
-    console.log(RAND_NUM);
-},3000);
+const RAND_NUM = Math.floor(Math.random() * 800);
+let coord_x = 0;
+let coord_y = 0;
 
 const colors = [
     "#ff3838",
@@ -20,13 +17,19 @@ const colors = [
     "#7efff5",
 ]
 
-function onMouse(event){
-    ctx.beginPath();
-    ctx.moveTo(RAND_NUM,RAND_NUM);
-    const color = colors[Math.floor(Math.random() * colors.length)];
-    ctx.strokeStyle = color;
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
+function onMouse(event){ // 마우스 이동했을때 동작하는 함수
+    ctx.beginPath(); // 경로시작.
+    ctx.moveTo(coord_x, coord_y); // 전역변수 coord_x, coord_y에서 시작.
+    const color = colors[Math.floor(Math.random() * colors.length)]; // 칼라는 칼라배열에서 하나 가져오고.
+    ctx.strokeStyle = color; // 선 색깔 가져온 칼라로 지정.
+    ctx.lineTo(event.offsetX, event.offsetY); // 현재x좌표,y좌표로 선 이동.
+    ctx.stroke(); // 선 긋기.
 }
 
+function onClick(event){
+    coord_x = event.offsetX;
+    coord_y = event.offsetY;
+}
+
+canvas.addEventListener("click", onClick);
 canvas.addEventListener("mousemove", onMouse);
