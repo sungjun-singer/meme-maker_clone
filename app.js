d5584c63035ba670933c13dcb1d04454e77fd8ec
kimsungjun <kis07150@gmail.com>
2023-02-18 16:04:54 +0900

    별로 안귀엽고 무서운 사람 완성

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ const ctx = canvas.getContext("2d")
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
-ctx.lineWidth = 2;
-ctx.fillRect(300, 300, 50, 100);
-ctx.fillRect(200,200,200,20);
+ctx.fillRect(200, 200, 15, 100);
+ctx.fillRect(360, 200, 15, 100);
+ctx.fillRect(260, 200, 60, 200);
 
-ctx.moveTo(200, 200);
-ctx.lineTo(325, 100);
-ctx.lineTo(450, 200);
+ctx.arc(292, 125, 50, 0, 2 * Math.PI);
+ctx.fill();
+
+ctx.beginPath();
+ctx.arc(270, 105, 10, Math.PI, 2*Math.PI);
+ctx.arc(314, 105, 10, Math.PI, 2*Math.PI);
+ctx.fillStyle = "white";
+ctx.fill();
+
+ctx.beginPath();
+ctx.arc(292, 135, 30, 0, Math.PI);
 ctx.fill();
