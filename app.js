02bd23e24d6ba3b76023ec2842eead0dd3e3b801
kimsungjun <kis07150@gmail.com>
2023-02-19 14:03:09 +0900

    선 크기 조절 슬라이드바 추가

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
+const lineWidth = document.querySelector("#line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d")
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 let isPainting = false;
 function onMove(event){
     if(isPainting){
         ctx.lineTo(event.offsetX, event.offsetY);
-        ctx.fill();
+        ctx.stroke();
         return;
     }
-
+    ctx.beginPath();
     ctx.moveTo(event.offsetX, event.offsetY);
 }
 
@@ -21,7 +22,14 @@ function startPainting(){
 function cancelPainting(){
     isPainting = false;
 }
+
+function onLineWidthChange(event){
+    ctx.lineWidth = event.target.value
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting)
+
+lineWidth.addEventListener("change", onLineWidthChange);
